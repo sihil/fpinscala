8c93b2798c85ea7e25d980ec251d14589a88fe38
Simon Hildrew <simon.hildrew@theguardian.com>
2017-01-13 11:23:31 +0000

    checkpoint

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,19 +50,115 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => throw new IllegalArgumentException("Can't do tail of Nil")
+      case Cons(_, tail) => tail
+    }
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = {
+    l match {
+      case Nil => Cons(h, Nil)
+      case Cons(_, tail) => Cons(h, tail)
+    }
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n == 0)
+      l
+    else {
+      l match {
+        case Nil => Nil
+        case Cons(_, tail) => drop(l, n-1)
+      }
+    }
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      case Cons(head, tail) if f(head) => dropWhile(tail, f)
+      case other => other
+    }
+  }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def init[A](l: List[A]): List[A] = {
+    l match {
+      case Cons(_, Nil) => Nil
+      case Cons(head, tail) => Cons(head, init(tail))
+      case Nil => throw new IllegalArgumentException("Can't do init of Nil")
+    }
+  }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, count) => count+1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = {
+    l match {
+      case Nil => z
+      case Cons(head, tail) => foldLeft(tail, f(z, head))(f)
+    }
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def sum3(ns: List[Int]): Int = List.foldLeft(ns, 0)(_+_)
+
+  def product3(ns: List[Double]): Double = List.foldLeft(ns, 1.0)(_*_)
+
+  def length2(ns: List[_]): Int = List.foldLeft(ns, 0)((count, _) => count + 1)
+
+  def reverse[A](ns: List[A]): List[A] = List.foldLeft[A, List[A]](ns, Nil)((acc, h) => Cons(h, acc))
+
+  // TODO - foldLeft in terms of foldRight and vv.
+
+  def append2[A](left: List[A], right: List[A]): List[A] = List.foldRight(left, right)((item, tail) => Cons(item, tail))
+
+  def concatenate[A](lists: List[List[A]]): List[A] = {
+    List.foldRight[List[A], List[A]](lists, Nil)((list, tail) => append2(list, tail))
+  }
+
+  def addOne(list: List[Int]): List[Int] = List.foldRight[Int, List[Int]](list, Nil)((head, tail) => Cons(head + 1, tail))
+
+  def doubleToString(list: List[Double]): List[String] =
+    List.foldRight[Double, List[String]](list, Nil)((head, tail) => Cons(head.toString, tail))
+
+  def map[A,B](as: List[A])(f: A => B): List[B] =
+    List.foldRight[A, List[B]](as, Nil)((head, tail) => Cons(f(head), tail))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] =
+    List.foldRight[A, List[A]](as, Nil)((head, tail) => if (f(head)) Cons(head, tail) else tail )
+
+  def removeOdds(ints: List[Int]) = filter(ints)(_ % 2 == 0)
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = List.foldRight[A, List[B]](as, Nil)((item, acc) => append2(f(item), acc))
+
+  def filter2[A](as: List[A])(f: A => Boolean) = flatMap(as)(a => if (f(a)) Cons(a, Nil) else Nil)
+
+  def sumLists(as: List[Int], bs: List[Int]): List[Int] = {
+    (as, bs) match {
+      case (Cons(aHead, aTail), Cons(bHead, bTail)) => Cons(aHead + bHead, sumLists(aTail, bTail))
+      case _ => Nil
+    }
+  }
+
+  def zipWith[A, B](as: List[A], bs: List[B]): List[(A, B)] = {
+    (as, bs) match {
+      case (Cons(aHead, aTail), Cons(bHead, bTail)) => Cons((aHead, bHead), zipWith(aTail, bTail))
+      case _ => Nil
+    }
+  }
+
+  /* True if sup starts with the items in sub */
+  def startsWith[A](sup: List[A], sub: List[A]): Boolean = {
+    (sup, sub) match {
+      case (_, Nil) => true
+      case (Cons(aHead, aTail), Cons(bHead, bTail)) => aHead == bHead && startsWith(aTail, bTail)
+      case _ => false
+    }
+  }
+
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {
+    List.startsWith(sup, sub) || (sup match {
+      case Cons(_, tail) => hasSubsequence(tail, sub)
+      case Nil => false
+    })
+  }
 }
--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -1,5 +1,7 @@
 package fpinscala.gettingstarted
 
+import scala.annotation.tailrec
+
 // A comment!
 /* Another comment */
 /** A documentation comment */
@@ -36,7 +38,14 @@ object MyModule {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    @tailrec
+    def go(prev2: Int, prev1: Int, i: Int): Int = {
+      if (i == 0) prev2
+      else go(prev1, prev1 + prev2, i-1)
+    }
+    go(0, 1, n)
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
@@ -140,7 +149,14 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @tailrec
+    def isSortedRec(index: Int): Boolean = {
+      if (as.size - index < 2) true
+      else gt(as(index), as(index+1)) && isSortedRec(index + 1)
+    }
+    isSortedRec(0)
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
@@ -153,13 +169,13 @@ object PolymorphicFunctions {
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    (a: A) => (b: B) => f(a, b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a: A, b: B) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -174,5 +190,5 @@ object PolymorphicFunctions {
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    (a: A) => f(g(a))
 }
